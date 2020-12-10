20758e1bcb432abad227e478dc09a81698cfd07d
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-12-10 17:07:07 +0000

    Project was added

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+// Page Elements
+const input = document.querySelector('.search__field');
+const submitBtn = document.querySelector('.search__btn');
+
+
+// Add AJAX functions here:
+const getRecipe = async () => {
+    
+    }
+
+
+const getOneRecipe = async (id) => {
+    
+    }
+
+
+
---
+++ js/searchView.js
@@ -0,0 +1,7 @@
+
+const renderRecipe = recipe => {
+    
+    
+
+};
+
